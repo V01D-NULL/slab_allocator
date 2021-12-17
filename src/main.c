0a560b34c8a08024c7235b1b7cb071a129c68f04
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2021-12-17 20:05:24 +0100

    testing: create more dummy caches

--- src/main.c
+++ src/main.c
@@ -2,13 +2,19 @@
 #include "slab.h"
 #include <stdlib.h>
 
-void ctor(size_t foo) {}
+void ctor1(size_t foo) {}
+void ctor2(size_t foo) {}
 
 int main(int argc, char const *argv[])
 {
     slab_init();
-    slab_cache_t *dummy_cache = slab_cache_create("dummy_cache", 20, ctor);
-    slab_traverse_cache(dummy_cache); // Note: Everything except for the cache descriptor string and the cache size
+
+    slab_cache_t *dummy_cache1 = slab_cache_create("first dummy_cache", 20, ctor1);
+    slab_traverse_cache(dummy_cache1); // Note: Everything except for the cache descriptor string and the cache size
+                                      // should be 0 since there have been no calls to slab_cache_alloc().
+    
+    slab_cache_t *dummy_cache2 = slab_cache_create("second dummy_cache", sizeof(int), ctor1);
+    slab_traverse_cache(dummy_cache2); // Note: Everything except for the cache descriptor string and the cache size
                                       // should be 0 since there have been no calls to slab_cache_alloc().
 
     return 0;
