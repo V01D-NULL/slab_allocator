9e05bced6d15fc444cc284aa1fcf86c0cf0dc3ed
V01D-NULL <timster23@protonmail.com>
2022-01-13 22:37:55 -0600

    Add test case + bug/fixme comment with a theory on what went wrong and why

--- src/main.c
+++ src/main.c
@@ -25,15 +25,27 @@ int main(int argc, char const *argv[])
         exit(1);
     }
     
+    slab_cache_t *never_gonna_give_you_up = slab_create_cache("get rickrolled :D", size, 3, NULL, NULL);
+    if (!never_gonna_give_you_up)
+    {
+        LOG("cache creation failed\n");
+        exit(1);
+    }
+
     // Demo: Allocate memory from a partial slab. Take a free slab if
     // the partial slab is empty. Exits the program if no free slab is available.
     // for (;;)
     //     if (slab_alloc(fs, NULL, size) == NULL)
     //         break;
 
+    // FIXME: print_caches doesn't print 'dummy_cache1', something about slab_cache_create breaks or removes a head node (?)
     print_caches();
-    slab_destroy(fs);
-    slab_destroy(dummy_cache1);
+    // slab_destroy(dummy_cache1);
+    // slab_destroy(fs);
+    // print_caches();
+
+    
+    // slab_alloc(fs, NULL, size);
 
     return 0;
 }
