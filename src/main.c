eb04d0cfb3de54fd0c4058c3e01b02dabe59b18f
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-01-16 22:17:31 +0100

    slab_free works (test)

--- src/main.c
+++ src/main.c
@@ -27,10 +27,35 @@ int main(int argc, char const *argv[])
 
     // Demo: Allocate memory from a partial slab. Take a free slab if
     // the partial slab is empty. Exits the program if no free slab is available.
+	
+	void *ptr = slab_alloc(dummy_cache1, NULL, size);
+
     for (;;)
         if (slab_alloc(dummy_cache1, NULL, size) == NULL)
             break;
 	
+	LOGV("starting slab_traverse_cache\n");
+	slab_traverse_cache(dummy_cache1);
+	LOGV("slab_traverse_cache done\n\n");
+
+	LOGV("starting print_slabs (free)\n");
+	print_slabs(dummy_cache1->free);
+	LOGV("print_slabs (free) done\n\n");
+
+	LOGV("starting print_slabs (used)\n");
+	print_slabs(dummy_cache1->used);
+	LOGV("print_slabs (used) done\n\n");
+
+	LOGV("starting print_slabs (partial)\n");
+	print_slabs(dummy_cache1->partial);
+	LOGV("print_slabs (partial) done\n\n");
+
+	LOGV("starting print_caches\n");
+	print_caches();
+	LOGV("print_caches done\n\n");
+
+	slab_free(dummy_cache1, ptr);
+
 	LOGV("starting slab_traverse_cache\n");
 	slab_traverse_cache(dummy_cache1);
 	LOGV("slab_traverse_cache done\n\n");
