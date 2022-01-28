fdea3c3383a2eb024be388c80872ec042c3b3314
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-01-28 23:11:41 +0100

    making cache paramter of slab_alloc optional

--- src/main.c
+++ src/main.c
@@ -9,68 +9,66 @@ int main(int argc, char const *argv[])
 {
     slab_init();
 
-    // void *ptr = slab_alloc(NULL, 2);
-
-    // for (;;)
-    //     if (slab_alloc(NULL, 2) == NULL)
-    //         break;
-
-    slab_cache_t *dummy_cache1 = NULL;
-    slab_cache_t *dummy_cache2 = NULL;
-    slab_cache_t *dummy_cache3 = NULL;
-    slab_cache_t *dummy_cache4 = NULL;
-
-    if ((dummy_cache1 = slab_create_cache(2, 3)) == NULL)
-    {
-        fprintf(stderr, "slab_cache_create returned NULL\n");
-        exit(1);
-    }
-
-    LOG("breakpoint 0.1\n");
-
-    if ((dummy_cache2 = slab_create_cache(4, 3)) == NULL)
-    {
-        fprintf(stderr, "slab_cache_create returned NULL\n");
-        exit(1);
-    }
-
-    LOG("breakpoint 0.2\n");
-
-    if ((dummy_cache3 = slab_create_cache(8, 3)) == NULL)
-    {
-        fprintf(stderr, "slab_cache_create returned NULL\n");
-        exit(1);
-    }
-
-    LOG("breakpoint 0.3\n");
-
-    if ((dummy_cache4 = slab_create_cache(16, 3)) == NULL)
-    {
-        fprintf(stderr, "slab_cache_create returned NULL\n");
-        exit(1);
-    }
-
-    LOG("breakpoint 0.4\n");
-
-    print_caches();
-
-    LOG("breakpoint 1\n");
-
-    void *ptr = slab_alloc(dummy_cache1, 2);
-
     for (;;)
-        if (slab_alloc(dummy_cache1, 2) == NULL)
+        if (slab_alloc(NULL, 2) == NULL)
             break;
 
-    LOG("breakpoint 2\n");
+    // slab_cache_t *dummy_cache1 = NULL;
+    // slab_cache_t *dummy_cache2 = NULL;
+    // slab_cache_t *dummy_cache3 = NULL;
+    // slab_cache_t *dummy_cache4 = NULL;
 
-    slab_destroy(dummy_cache4);
+    // if ((dummy_cache1 = slab_create_cache(2, 3)) == NULL)
+    // {
+    //     fprintf(stderr, "slab_cache_create returned NULL\n");
+    //     exit(1);
+    // }
 
-    LOG("breakpoint 3\n");
+    // LOG("breakpoint 0.1\n");
 
-    print_caches();
+    // if ((dummy_cache2 = slab_create_cache(4, 3)) == NULL)
+    // {
+    //     fprintf(stderr, "slab_cache_create returned NULL\n");
+    //     exit(1);
+    // }
 
-    LOG("breakpoint 4\n");
+    // LOG("breakpoint 0.2\n");
+
+    // if ((dummy_cache3 = slab_create_cache(8, 3)) == NULL)
+    // {
+    //     fprintf(stderr, "slab_cache_create returned NULL\n");
+    //     exit(1);
+    // }
+
+    // LOG("breakpoint 0.3\n");
+
+    // if ((dummy_cache4 = slab_create_cache(16, 3)) == NULL)
+    // {
+    //     fprintf(stderr, "slab_cache_create returned NULL\n");
+    //     exit(1);
+    // }
+
+    // LOG("breakpoint 0.4\n");
+
+    // print_caches();
+
+    // LOG("breakpoint 1\n");
+
+    // void *ptr = slab_alloc(dummy_cache1, 2);
+
+    // for (;;)
+    //     if (slab_alloc(dummy_cache1, 2) == NULL)
+    //         break;
+
+    // LOG("breakpoint 2\n");
+
+    // slab_destroy(dummy_cache4);
+
+    // LOG("breakpoint 3\n");
+
+    // print_caches();
+
+    // LOG("breakpoint 4\n");
 
     return 0;
 }
